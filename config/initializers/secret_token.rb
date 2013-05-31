f78687685d8f4353cf8dd62d11095690838bc947
Digital-Learner <digital.learning.journeysatgmail.com>
2013-05-31 12:22:11 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anagrams::Application.config.secret_token = 'c8d406878557240352b22ccbd5fcd02034d616d88ba7f74ad084e679ae079c018a2eb7dc4f50039a098902f17d60f65bb5648c32d4314f1c317addd5ce618d8a'
